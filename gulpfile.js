62949f3c5ea352c28d807d829e7426d76eadf128
shivam <jalotrashivam9@gmail.com>
2019-02-20 22:43:15 +0530

    Using gulp as a live server thus included it in depencies in package.json file

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  webserver = require('gulp-webserver');
+
+gulp.task('js', function() {
+  gulp.src('build/js/**/*');
+});
+
+gulp.task('html', function() {
+  gulp.src('build/*.html');
+});
+
+gulp.task('css', function() {
+  gulp.src('build/css/*.css');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('build/js/**/*', ['js']);
+  gulp.watch('build/css/*.css', ['css']);
+  gulp.watch(['build/*.html',
+    'build/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('build')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
